fd4ecae675a07258249d95d4ee3f3ccc3ad32774
candiceywu <candice.ying.wu@gmail.com>
2021-08-17 17:21:08 -0700

    added timer

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 // variables
 var highscores = document.querySelector("#highscores");
-var timerCountdown = document.querySelector(".timer-count");
+var timeCount = document.querySelector(".timer-count");
 var startButton = document.querySelector("#start");
 var nextButton = document.querySelector("#next-button");
 var main = document.querySelector(main);
 var questionToDisplay = document.querySelector("#description");
-var count = document.querySelector("#timer-count");
+var quizInstructions = document.querySelector("#instructions");
 
 
 // multiple choice questions
@@ -13,12 +13,11 @@ var mc1 = document.querySelector("#mc1");
 var mc2 = document.querySelector("#mc2");
 var mc3 = document.querySelector("#mc3");
 var mc4 = document.querySelector("#mc4");
-var answer = document.querySelector("#answer");
 var showAnswer = document.querySelector("#show-answer");
 var finalScore = [];
-var winCounter = 0;
-var loseCounter = 0;
-var isCorrect = false;
+var winCount = 0;
+var isCorrect = "";
+var loseCount = 0;
 var currentQuestions = "";
 var questionNum = 0;
 var currentQuestion = "";
@@ -28,7 +27,7 @@ document.getElementById("introduction").textContent = "Let's test your coding kn
 
 // question bank (5 questions)
 var questionSet1 = {
-    prompt: "Where in HTML do you link a Javascript file?",
+    qPrompt: "Where in HTML do you link a Javascript file?",
     option1: "1. In the header",
     option2: "2. At the bottom right before the closing tag body",
     option3: "3. After closing tag body",
@@ -37,7 +36,7 @@ var questionSet1 = {
 };
 
 var questionSet2 = {
-    prompt: "What does .shift do?",
+    qPrompt: "What does .shift do?",
     option1: "1. Removes the last element from an array",
     option2: "2. Adds an element to the end of the array",
     option3: "3. Removes the first element from an array",
@@ -46,7 +45,7 @@ var questionSet2 = {
 };
 
 var questionSet3 = {
-    prompt: "What is NOT a primitive data type?",
+    qPrompt: "What is NOT a primitive data type?",
     option1: "1. Null",
     option2: "2. String",
     option3: "3. Number",
@@ -55,7 +54,7 @@ var questionSet3 = {
 };
 
 var questionSet4 = {
-    prompt: "What does API stand for?",
+    qPrompt: "What does API stand for?",
     option1: "1. Application Programming Interface",
     option2: "2. Associated Platform Internet",
     option3: "3. Applicable Progam Interaction",
@@ -64,7 +63,7 @@ var questionSet4 = {
 };
 
 var questionSet5 = {
-    prompt: "What is a console log?",
+    qPrompt: "What is a console log?",
     option1: "1. It is a way to display a message on the debugging console",
     option2: "2. It is a method",
     option3: "3. It is a variable",
@@ -74,11 +73,13 @@ var questionSet5 = {
 
 var questionBank = [questionSet1, questionSet2, questionSet3, questionSet4, questionSet5];
 var currentQuestion = questionBank[questionNum];
-console.log(currentQuestion.prompt);
+console.log(currentQuestion.qPrompt);
 
 function updateApp() {
     currentQuestion = questionBank[questionNum];
-    questionToDisplay.textContent = currentQuestion.prompt;
+    // console.log(currentQuestion);
+    // console.log(questionNum);
+    questionToDisplay.textContent = currentQuestion.qPrompt;
     mc1.textContent = currentQuestion.option1;
     mc2.textContent = currentQuestion.option2;
     mc3.textContent = currentQuestion.option3;
@@ -86,7 +87,7 @@ function updateApp() {
     return currentQuestion;
 };
 
-// when you click start button, it should disappear
+// when you click start button, start button should disappear
 // mc buttons 1-4 should be hidden and when button is clicked, they should show up
 // start button will then disappear (hidden? set attribute?)
 // Description will need to turn into question prompt
@@ -95,6 +96,7 @@ document.addEventListener("click", function (event) {
     if (event.target === startButton) {
         updateApp();
         startButton.setAttribute("style", "display: none;");
+        quizInstructions.setAttribute("style", "display: none");
         mc1.setAttribute("style", "display: block;");
         mc2.setAttribute("style", "display: block;");
         mc3.setAttribute("style", "display: block;");
@@ -102,60 +104,51 @@ document.addEventListener("click", function (event) {
     };
 });
 
-// document.addEventListener("click", function (event) {
-//     if (event.target === nextButton) {
-//         updateApp();
-//     };
-// });
-
-// event listener 
+// event listener for correct/incorrect buttons
 document.addEventListener("click", function (event) {
     if (event.target === mc1 || event.target === mc2 || event.target === mc3 || event.target === mc4) {
         if (event.target.textContent[0] == currentQuestion.answer) {
             showAnswer.textContent = "Correct!";
-            // delay function?
+            isCorrect = "Correct!";
             questionNum++;
-            updateApp();
             finalScore++;
-            console.log(questionNum);
+            updateApp();
+            console.log(isCorrect);
         } else {
             showAnswer.textContent = "Incorrect!";
+            isCorrect = "Incorrect!";
             questionNum++;
+            count -=10;
             updateApp();
-            console.log(questionNum);
+            console.log(isCorrect);
         };
+    }
+
+});
+
+// timer variables
+var count = 60;
+var timer;
+
+// starts timer when "start" button is clicked
+document.addEventListener("click", function (event) {
+    if (event.target === startButton) {
+        updateApp();
+        startTimer();
     }
 });
 
-// if current question is greater than current, do not increment but still allow to move on
+// function that sets timer
+function startTimer() {
+    timer = setInterval(function () {
+        count--;
+        console.log(count);
+        timeCount.textContent = count;
 
-// // timer
-// var count = 60;
-// var timer;
+        if (count === 0) {
+            // stops execution of action at set interval
+            clearInterval(timerInterval);
+        }
 
-// //need to add eventListener for when "start" button is clicked, the counter begins
-// document.addEventListener("click", function (event) {
-//     if (event.target === startButton) {
-//         updateApp();
-//         function startTimer() {
-//     // sets timer
-//     timer = setInterval(function () {
-//         timerCount--;
-//         timerElement.textContent = timerCount;
-//         if (timerCount >= 0) {
-//             // tests if answer is incorrect
-//             if (isCorrect) {
-//                 timerCount - 10;
-//             }
-//         }
-
-//         if (secondsLeft === 0) {
-//             // stops execution of action at set interval
-//             clearInterval(timerInterval);
-//         }
-
-//     }, 60000); //one minute 
-// }
-
-// startTimer();
-
+    }, 1000); // timer is a loop, iterates 
+}
